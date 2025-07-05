213ad2bb3a82a2a00bb05f20074f678c2630a7a3
CioFlinGar <coder.walidhasan@gmail.com>
2025-07-06 00:50:03 +0600

    make views to take post request from et form and save it to database

--- et_app/views.py
+++ et_app/views.py
@@ -4,5 +4,10 @@ from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
     expense_form = ExpenseForm()
     return render(request, "et_app/index.html", {"expense_form":expense_form})
