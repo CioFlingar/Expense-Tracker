9554a3ef68acff6630304253b6270bbda47ebc50
CioFlinGar <coder.walidhasan@gmail.com>
2025-07-05 23:00:16 +0600

    Inital expense tracker app storage commit

---
+++ et_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .forms import ExpenseForm
+
+
+# Create your views here.
+def index(request):
+    expense_form = ExpenseForm()
+    return render(request, "et_app/index.html", {"expense_form":expense_form})
