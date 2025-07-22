16ecf0e592353c15601e8f2a660fb52e0aa7456b
CioFlinGar <coder.walidhasan@gmail.com>
2025-07-22 22:22:43 +0600

    get all the data and return when rendering

--- et_app/views.py
+++ et_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
-
+from .models import Expense
 
 # Create your views here.
 def index(request):
@@ -9,5 +9,6 @@ def index(request):
         if expense.is_valid():
             expense.save()
 
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request, "et_app/index.html", {"expense_form":expense_form})
+    return render(request, "et_app/index.html", {"expense_form":expense_form, "expenses": expenses})
