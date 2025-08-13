d5a9cdec0da9ebdfb7202c1953d8eefa686fdfec
CioFlinGar <coder.walidhasan@gmail.com>
2025-08-13 11:07:21 +0600

    charts added

--- et_app/views.py
+++ et_app/views.py
@@ -29,8 +29,16 @@ def index(request):
 
     daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum("amount"))
 
+    categorical_sums = Expense.objects.filter().values("category").order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
+
+
     expense_form = ExpenseForm()
-    return render(request, "et_app/index.html", {"expense_form":expense_form, "expenses": expenses, "total_expenses": total_expenses, "yearly_sum":yearly_sum, "monthly_sum":monthly_sum, "weekly_sum":weekly_sum})
+    return render(request, "et_app/index.html", {"expense_form":expense_form, "expenses": expenses, "total_expenses": total_expenses, "yearly_sum":yearly_sum, "monthly_sum":monthly_sum, "weekly_sum":weekly_sum,
+    "daily_sums": daily_sums,
+    "categorical_sums": categorical_sums})
+
+
 
 def edit(request, id):
     expense = Expense.objects.get(id=id)
