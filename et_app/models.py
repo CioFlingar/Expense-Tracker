1432f6fd776a6d6896fb6457c4aabd541f0ee083
CioFlinGar <coder.walidhasan@gmail.com>
2025-08-14 22:18:10 +0600

    change date to creation date rather than general

--- et_app/models.py
+++ et_app/models.py
@@ -9,4 +9,4 @@ class Expense(models.Model):
     name = models.CharField(max_length=100)
     amount = models.IntegerField()
     category = models.CharField(max_length=50)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now_add=True)
